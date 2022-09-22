bf2514e9b7211e7d02a56e4eecf40d636a7f05c8
manymusa <adanalin113@gmail.com>
2022-09-22 12:43:16 -0400

    commit tree bdc2764bd4c9ee41d2f79cd3d54433e93292f15b

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if(!props.isPublished){
+    //hide unpublished content
+    //return null means "don't display any DOM elements"
+    return null
+  } else{
+    //show published content 
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead}</p>
+      </div>
+    )
+  }
+  // return <div id="blog-content">{props.articleText}</div>;
+
 }
 
 export default BlogContent;
